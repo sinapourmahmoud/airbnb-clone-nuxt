ac3aabf6da5cec34fba486ae5631f6ee3b0c3cee
sinapourmahmoud <sinapoormahmood@gmail.com>
2023-07-07 12:15:30 +0330

    handle loading

--- app.vue
+++ app.vue
@@ -1,11 +1,15 @@
 <template>
   <NuxtLayout>
+    <Loading v-if="loading" />
     <NuxtPage />
   </NuxtLayout>
 </template>
 <script setup lang="ts">
 let { initAuth } = useAuth();
+let loading = ref(true);
 onMounted(async () => {
+  loading.value = true;
   await initAuth();
+  loading.value = false;
 });
 </script>
---
+++ components/Loading.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="w-full h-screen fixed inset-0 bg-white z-50 flex items-center justify-center"
+  >
+    <img src="./../assets/logo2.png" class="h-16 animate-ping" alt="logo" />
+  </div>
+</template>
